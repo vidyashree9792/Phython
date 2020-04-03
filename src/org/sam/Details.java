1a51485cb8e9df4a6862ac8d17ceb07624da3052
vidyashree9792 <vshree9792@gmail.com>
2020-04-03 21:03:27 +0530

    Project

---
+++ src/org/sam/Details.java
@@ -0,0 +1,12 @@
+package org.sam;
+
+public class Details {
+	  public static void main(String[] args) {
+	    	 int a=10;
+	    		if (a%2==0) {
+	    			System.out.println("even");
+	    		} else {
+	    	        System.out.println("odd");
+	    		}
+		}
+}
